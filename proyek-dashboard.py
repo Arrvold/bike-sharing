b54c05f27628c71932e5acfe271ebd889c02ff1c
Arjikusna M <162129789+Arrvold@users.noreply.github.com>
2024-10-04 22:57:53 +0700

    Update proyek-dashboard.py

--- proyek-dashboard.py
+++ proyek-dashboard.py
@@ -1,7 +1,8 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 # Fungsi untuk menampilkan total penggunaan sepeda berdasarkan hari kerja dan akhir pekan
 def tampilan_penyewaan_cuaca(hour_df):
